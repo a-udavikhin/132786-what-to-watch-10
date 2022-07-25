19f33ea38cc6062695c6833f775141fe4c9d7f9a
Anatoly Udavikhin <anatoly.udavikhin@gmail.com>
2022-07-25 17:53:33 +0300

    Добавляет новые компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppProps = {
+  promoFilmTitle: string,
+  promoFilmGenre: string,
+  promoFilmYear: string
+}
+
+function App({promoFilmTitle, promoFilmGenre, promoFilmYear}: AppProps): JSX.Element {
+  return (<MainScreen promoFilmTitle={promoFilmTitle} promoFilmGenre={promoFilmGenre} promoFilmYear={promoFilmYear}/>);
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,8 +6,20 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const Settings = {
+  PROMO_FILM : {
+    TITLE: 'The Grand Budapest Hotel',
+    GENRE: 'Drama',
+    YEAR: '2014'
+  }
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      promoFilmTitle={Settings.PROMO_FILM.TITLE}
+      promoFilmGenre={Settings.PROMO_FILM.GENRE}
+      promoFilmYear={Settings.PROMO_FILM.YEAR}
+    />
   </React.StrictMode>,
 );
