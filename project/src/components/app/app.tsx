5bc194428f71691b6e613b8f0b49d0e3334a3a69
Anatoly Udavikhin <anatoly.udavikhin@gmail.com>
2022-07-28 13:01:13 +0300

    Добавляет маршрутизацию и компонент страницы 404

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import PrivateRoute from '../private-route/private-route';
 import MainScreen from '../../pages/main-screen/main-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import FilmScreen from '../../pages/film-screen/film-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppProps = {
   promoFilm: {
@@ -9,7 +18,44 @@ type AppProps = {
 }
 
 function App({promoFilm}: AppProps): JSX.Element {
-  return (<MainScreen promoFilm={promoFilm}/>);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen promoFilm={promoFilm}/>}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignInScreen />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth} >
+              <MyListScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<FilmScreen />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewScreen />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
