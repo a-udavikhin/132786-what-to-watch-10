a9e00790a059fb67f577d59c5143034046abefa4
Anatoly Udavikhin <anatoly.udavikhin@gmail.com>
2022-08-09 20:32:37 +0300

    Завершает создание компонента video-player

--- project/src/components/video-player/video-player.tsx
+++ project/src/components/video-player/video-player.tsx
@@ -1,5 +1,6 @@
 import {useRef, useEffect, useState} from 'react';
 import { Film } from '../../types/film';
+import {AUTO_PLAY_DELAY_MS} from '../../const';
 
 type VideoPlayerProps = {
   noSound: boolean,
@@ -7,29 +8,43 @@ type VideoPlayerProps = {
 }
 
 function VideoPlayer({noSound, film}: VideoPlayerProps): JSX.Element {
-  //const [/*isLoading*/, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [isPlaying, setIsPlaying] = useState(false);
-
+  const [playerHoverTimer, setPlayerHoverTimer] = useState<ReturnType<typeof setTimeout> | null>(null);
 
   const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  function playerMouseEnterHandler(): void {
+    if (!isPlaying && !isLoading) {
+      setPlayerHoverTimer(setTimeout(() => setIsPlaying(true), AUTO_PLAY_DELAY_MS));
+
+    }
+  }
+
+  function playerMouseLeaveHandler(): void {
+    setIsPlaying(false);
+    clearTimeout(Number(playerHoverTimer));
+  }
 
   useEffect(() => {
     if (videoRef.current === null) {
       return;
     }
 
-    //videoRef.current.addEventListener('loadeddata', () => setIsLoading(false));
-    //videoRef.current.addEventListener('mouseenter', () => setIsPlaying(!isPlaying));
+    videoRef.current.addEventListener('loadeddata', () => setIsLoading(false));
+
     if (isPlaying) {
       videoRef.current.play();
     }
 
-    videoRef.current.pause();
+    if (!isPlaying) {
+      videoRef.current.load();
+    }
 
-  });
+  }, [isPlaying]);
 
   return (
-    <div onMouseEnter = {() => setIsPlaying(!isPlaying)} className="small-film-card__image">
+    <div onMouseEnter={playerMouseEnterHandler} onMouseLeave={playerMouseLeaveHandler} className="small-film-card__image">
       <video
         src={film.src}
         ref={videoRef}
